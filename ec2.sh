46f00ceb9045cd61a04ad95ae0d8f7a7d38b4ba1
pathipatireshma <pathipati.reshma@gmail.com>
2022-05-17 12:09:30 +0530

    ec2

--- ec2.sh
+++ ec2.sh
@@ -23,8 +23,8 @@ Instance_name="$1-dev"
 # Creating Instance
  aws ec2 run-instance --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specification "ResourceType-instance,Tags=[{key=Name,value=${Instance_name}}]" "ResourceType=spot-instance-request,Tags=[{Key=Name,Value=${Instance_name}}]" --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistence}" &>>LOG
     echo -e "\e[1m Instance created\e[0m"
-  else
-    echo "Instance ${Instance_name} is already exists, Hence not creating"
+#  else
+#    echo "Instance ${Instance_name} is already exists, Hence not creating"
 
 
 #    aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"  --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}"  &>>$LOG
