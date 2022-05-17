6e348cb66137e46c72d9c4ce3212418ce251e946
pathipatireshma <pathipati.reshma@gmail.com>
2022-05-17 10:54:08 +0530

    ec2

--- ec2.sh
+++ ec2.sh
@@ -5,9 +5,17 @@ rm -f $LOG
 
 INSTANCE_CREATE() {
   Instance_name=$1
-  if[ -z "${Instance_name}" ]; then
+  if [ -z "${Instance_name}" ]; then
     echo -e "Instance name is requried"
     exit
   fi
 
 }
+
+#INSTANCE_CREATE() {
+#  INSTANCE_NAME=$1
+#  if [ -z "${INSTANCE_NAME}" ]; then
+#    echo -e "\e[1;33mInstance Name Argument is needed\e[0m"
+#    exit
+#  fi
+#  INSTANCE_NAME="$1-dev"
