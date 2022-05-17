fcf5fe31371cc603708e858f63e3b82f8c719fc6
pathipatireshma <pathipati.reshma@gmail.com>
2022-05-17 10:44:03 +0530

    ec2

--- ec2.sh
+++ ec2.sh
@@ -2,3 +2,12 @@
 
 LOG=/tmp/instance-create.log
 rm -f $LOG
+
+INSTANCE_CREATE() {
+  Instance_name=$1
+  if[ -z "${Instance_name}" ]; then
+    echo -e "Instance name is requried"
+    exit
+  fi
+
+}
